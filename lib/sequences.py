768d0217011e5f8561c7f587cee7d72f7fe3bd26
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2025-11-18 18:04:47 +0300

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # generate the first `length` Fibonacci numbers and print as a list
+    if length <= 0:
+        print([])
+        return
+
+    fib = [0]
+    if length == 1:
+        print(fib)
+        return
+
+    fib.append(1)
+    while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+
+    print(fib)
